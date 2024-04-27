a937cb2aec22822c6643b84084c7b0d5f72f8463
n00bcybot <freshmaker21@gmail.com>
2024-04-27 21:05:29 +0200

    More ideas on rendering sequentially with start-job and getting output to a textbox

--- snippets.ps1
+++ snippets.ps1
@@ -101,3 +101,154 @@ do {
 
 
 } until ($index -eq 6)
+
+
+#------------------------------------------------------------------------------------------------------------
+
+$nodeslist =  "fresh2", "fresh3"
+$sessions = @()
+$OutputDir = "\\fresh1\render\render"
+$FilePath = "\\fresh1\render\test.ma"
+$StartFrame = 1
+$EndFrame = 5
+$CameraName = "persp"
+
+$jobpool = [System.Collections.ArrayList]::new()
+foreach ($frame in $StartFrame..$EndFrame){
+$jobpool.Add("render -r arnold -rd $OutputDir -s $frame -e $frame -cam $CameraName $FilePath") | Out-Null
+}
+
+$creds = Get-Credential -UserName fresh
+
+foreach ($node in $nodeslist){
+
+    $session = New-PSSession -ComputerName $node -Credential $creds
+    $sessions += $session
+}
+#>
+do {
+foreach ($session in $sessions){
+
+    if (!(Invoke-Command -Session $session -ScriptBlock {Get-Process -Name "render" -ErrorAction SilentlyContinue})){
+        $job = $jobpool[0]
+        Invoke-Command -Session $session -ScriptBlock {
+           
+            New-PSDrive -Name "R" -PSProvider FileSystem -Root "\\fresh1\render" -Credential $using:creds -ErrorAction SilentlyContinue | Out-Null
+            $job = $using:job
+            Write-Output "Starting $job on $env:COMPUTERNAME"
+          $renderjob = Start-Job -Name $("job_$using:StartFrame") -ScriptBlock {Start-Process pwsh "-command $using:job" -NoNewWindow -RedirectStandardOutput "\\fresh1\render\render\log_$env:COMPUTERNAME.txt"}
+            
+        }
+        $computername = Invoke-Command -Session $session -ScriptBlock {$env:COMPUTERNAME}
+        $log = Get-Content -Path "\\fresh1\render\render\log_$computername.txt"
+        foreach($line in $log){if ($line -ne ""){Write-Output $line}}
+        
+        $jobpool.Remove($jobpool[0])
+        Start-Sleep 2
+
+     } else {
+           continue
+       }
+}
+} until ($jobpool.Count -eq 0)
+
+#-------------------------------------------------------------------------------------------------------
+
+
+
+# Use computers on the same subnet to copy files to shared storage folder
+#--------------------------------------------------------------------------
+
+# Get credentials to open session to all nodes
+# -----------------------------------------------------------------------
+$creds = Get-Credential -UserName fresh
+
+# Load list of nodes from file or variable
+#------------------------------------------------------------------------
+$NodeList = $null
+$NodeList = "fresh1", "fresh2"
+
+# Get the number of threads for each machine and store them in a PSObject
+# -----------------------------------------------------------------------
+#
+$NodeThreads = @()
+$TotalThreadCount = $null
+$sessions = @()
+$index = 0
+foreach ($node in $NodeList){
+
+    $session = New-PSSession -ComputerName $node -Credential $creds
+    $sessions += $session
+
+
+   $ThreadCount = Invoke-Command -Session $session -ScriptBlock {
+    
+          (Get-CimInstance -ClassName Win32_Processor).NumberOfCores
+    }
+    foreach ($i in 1..$ThreadCount){
+        $index++
+        $member = [pscustomobject]@{Index= $index; LocalIndex = $i; Node = $node}
+        $NodeThreads += $member
+    }
+        $TotalThreadCount += $ThreadCount
+}
+#
+
+$Count = 1..$TotalThreadCount
+$SourceFolder = "\\fresh1\render\Veeam\source"
+$BackupFolder = "\\fresh1\render\Veeam\backup"
+$LogFolder = "\\fresh1\render\Veeam\logs"
+$SourceList = Get-ChildItem -Path $SourceFolder 
+
+#------------------------------------------------------------------------------------------------
+# Splitting the list over the total amount of threads
+#-------------------------------------------------------------------------------------------------
+
+function Make-Lists {
+param (
+    [int]$number,
+    [string]$SourceFolder,
+    [int]$threadCount,
+    [string]$BackupFolder
+)
+
+$SourceList = Get-ChildItem -Path $SourceFolder
+for ($i = $number; $i -le $SourceList.Length; $i = $i + $threadCount){
+    $sourcePath = "$SourceFolder\$($SourceList[$i - 1].Name)"
+    $backupPath = "$BackupFolder\$($SourceList[$i - 1].Name)"
+    Write-Output $sourcePath
+    
+    } 
+
+}
+#----------------------------------------------------------------------------------------------------------
+# Make the lists
+# ---------------------------------------------------------------------------------------------------------
+
+$array = @()
+foreach ($num in $NodeThreads.Index){
+
+        $list = Make-Lists -number $num -SourceFolder $SourceFolder -threadCount $TotalThreadCount -BackupFolder $BackupFolder
+        
+        $node = $NodeThreads[$num - 1].node
+        $array += [pscustomobject]@{Index = $num; Node = $node; List = $list}
+    
+}
+
+Invoke-Command -Session $sessions -ScriptBlock {
+   
+    New-PSDrive -Name "R" -PSProvider FileSystem -Root "\\fresh1\render" -Credential $using:creds -ErrorAction SilentlyContinue | Out-Null
+    
+   
+    $array = $using:array
+    $localArray = $array | Where-Object {$_.Node -eq $env:COMPUTERNAME} | select List, Index
+    
+    $block = {
+        param($List, $Folder)
+        Copy-Item $list -Destination $Folder -Recurse
+    }
+Measure-Command {
+   foreach ($item in $localArray.Index){
+       Start-Job $block -ArgumentList @($array[$item -1].List, $using:BackupFolder) | Wait-Job
+   }}
+}
