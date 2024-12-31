20055764694548bff24760cb6cfb03935d87d382
n00bcybot <freshmaker21@gmail.com>
2024-12-31 15:21:46 +0100

    Added check for empty input and range check

--- wifipassword.ps1
+++ wifipassword.ps1
@@ -12,8 +12,13 @@ Write-Output "`nAvailable WiFi profiles:`n"
 foreach ($entry in $wifiprofiles){
 "{0} - {1}" -f ($wifiprofiles.IndexOf($entry) +1), $entry
 }
-
+"`n"
 # Get user input, use index to select profile from the list and return the password for it
-$input = Read-Host "`nSelect profile number to display password"
+$input = 0
+while ($input -eq $null -or $input -eq "") {
+    while ($input -gt $wifiprofiles.Count -or $input -lt 1){
+    $input = Read-Host "Please select number from the profiles list (between 1 and $($wifiprofiles.count))"
+    }
+}
 $pass = Get-WiFiPassword -WiFiProfile $wifiprofiles[$input -1]
 $pass
